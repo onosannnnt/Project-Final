10fd3f71ccf5608fff1a56ef8f7424c6e43b2166
martksk <krtsk2547@gmail.com>
2026-05-16 03:10:13 +0700

    fix lifelink 3

--- Assets/_Scripts/BaseScriptableObject/CorruptHealthEffect.cs
+++ Assets/_Scripts/BaseScriptableObject/CorruptHealthEffect.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CorruptHealthEffect", menuName = "ScriptableObjects/SkillEffect/CorruptHealthEffect")]
 public class CorruptHealthEffect : SkillEffect
 {
+    public enum CorruptAction { Increase, Decrease }
+
+    [SerializeField] private CorruptAction action = CorruptAction.Increase;
     [SerializeField] private float flatAmount;
     [SerializeField, Range(0f, 1f)] private float percentOfMaxHP;
     [SerializeField] private bool targetCaster = true;
@@ -15,13 +18,13 @@ public class CorruptHealthEffect : SkillEffect
         float maxHP = effectTarget.GetStat(StatType.MaxHealth);
         float amount = flatAmount + (maxHP * percentOfMaxHP);
 
-        if (amount > 0)
+        if (action == CorruptAction.Increase)
         {
             effectTarget.GainCorruptedHealth(amount);
         }
-        else if (amount < 0)
+        else
         {
-            effectTarget.RemoveCorruptedHealth(-amount);
+            effectTarget.RemoveCorruptedHealth(amount);
         }
 
         return true;
